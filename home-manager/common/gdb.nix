5c2f6ac92e45434f7de76c62a22bbbcb6a374ff7
GreyStinger <jaydenandrews010@gmail.com>
2024-11-17 15:02:56 +0200

    refactor and move into .config

---
+++ home-manager/common/gdb.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+  home.file.".gdbinit".text = ''
+    set history save on
+
+    define n
+        next
+        refresh
+    end
+
+    define s
+        step
+        refresh
+    end
+
+    define si
+        stepi
+        refresh
+    end
+  '';
+}
---
+++ home-manager/common/xinit.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  home.file = {
+    ".xprofile".source = ./scripts/.xprofile;
+    ".xinitrc".source = ./scripts/.xinitrc;
+  };
+}
